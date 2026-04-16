b03f7504086aa3e7c063ba86b35d063c793a2be4
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-16 17:02:09 +0530

    user registration section is added

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const userRouter = express.Router();
 const UserModel = require('../models/usermodel');
+const bcrypt  = require('bcrypt');
+const saltrounds = 5;
 
+
+//registering the user
 userRouter.post('/register', async (req, res)=>{
     const{name, DOB, email, password, role } = req.body;
     if(!name || !DOB || !email || !password || !role){
         res.send(`Please enter correct creadiantials...`);
     }
     try {
+        //checking if user already exist or not
         const exstingUser = await UserModel.findOne({name, email});
         if(exstingUser){
             res.status(401).json({
@@ -15,6 +20,10 @@ userRouter.post('/register', async (req, res)=>{
             });
         }
 
+        //hashing the password
+        const hashedPassword = await bcrypt.hash(password, saltrounds);
+
+        //cerating new user
         const user = new UserModel({
             name,
             DOB,
@@ -22,11 +31,17 @@ userRouter.post('/register', async (req, res)=>{
             password,
             role
         });
+
+        await user.save();
+        console.log("User registerd successfully");
         res.status(201).json({
             message:`User registered successfully`
         });
-        await user.save();
-    } catch (error) {
         
+    } catch (error) {
+        console.log("error while registering the user")
+        res.status(500).json({
+            message:`Error while registering the user:${error.message}`
+        });
     }
 })
