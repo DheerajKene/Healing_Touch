98b36af5ee8ba604e0cc089b1c98867cbdab9c30
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-19 23:32:39 +0530

    Service route is created

---
+++ Backend/src/routes/service.route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const serviceModel = require('../models/service.model');
+const usermodel = require('../models/usermodel');
+const auth = require('../middleware/auth.middleware');
+
+const ServiceRouter = express.Router();
+
+//Show services got till the date:
+
+ServiceRouter.get('/got-served', async (req, res)=>{
+    try {
+        const services = await serviceModel.find(req.userId).populate();
+        if(!services){
+            res.status(403).json({
+                message:`Not served yet..`
+            });
+        }
+        res.status(200).json({
+            message:`Services fetch successfully`,
+            services
+        });
+    } catch (error) {
+        res.status(500).json({
+            message:`error found :${error.message}`
+        });
+    }
+    
+});
