b08a9278dc6f1eeb0993f76a9a86897f55b36581
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-19 23:10:36 +0530

    service model is updated

--- Backend/src/models/service.model.js
+++ Backend/src/models/service.model.js
@@ -7,9 +7,40 @@ const serviceSchema = mongoose.Schema({
         ref:`User`,
         required:true
     },
+    name:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    care_for:{
+        type:String,
+        required:true,
+        enum:["Arthritis","Cancer","Dementia","Diabetes","Stroke","Alzheimer’s Disease","Cardiovascular Disease","Traumatic Brain Injury","Parkinson’s Disease"]
+    },
     name_of_service:{
         type:String,
         required:true,
         enum:["Our Caregivers", "Attendant Services", "Nursing Services","Elderly Care","Doctor at Home","Physiotherapist","Rehabilitation at Home"]
+    },
+    age:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    service_starts:{
+        type:Date,
+        default:Date.now
     }
-})
+},
+{
+    version_key:false
+});
+
+const serviceModel = mongoose.model("service", serviceSchema);
+module.exports = serviceModel
