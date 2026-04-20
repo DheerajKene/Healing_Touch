2e09bd25ae135de9afa10ea862de567c6d9a689f
Dhiraj Kene <kdprakash1712@gmail.com>
2026-04-20 15:56:21 +0530

    context is added in frontend

---
+++ Frontend/src/context/Authcontext.jsx
@@ -0,0 +1,30 @@
+import{createContext, useState} from 'react'
+
+export const Authcontext = createContext();
+
+export const AuthcontextProvider = ()=>{
+    const [AuthDeatils, setAuthdetails] = useState({
+        isAuthenticated:false,
+        email:null,
+        token:null
+    });
+
+    const login = ({email, token})=>{
+        setAuthdetails({
+            isAuthenticated:true,
+            email,
+            token
+        });
+    }
+
+    const logout = ()=>{
+        setAuthdetails({
+            isAuthenticated:false,
+            email:null,
+            token:null
+        })
+      
+    }
+
+    return <Authcontext.Provider value={{AuthDeatils, login, logout}}>{childern}</Authcontext.Provider>
+}
